2f557d1ae31a4c7f437860e756db277b9c11074d
Liang Liu <lliu@uga.edu>
2024-10-13 18:35:06 +0800

    Add files via upload

--- src/betnet.jl
+++ src/betnet.jl
@@ -1,1 +1,17 @@
+module betnet
 
+    using Distributions
+    using BenchmarkTools
+    using Base.Threads: @spawn, @threads
+    using Random
+    using StatsBase
+    using FreqTables  
+      
+    const ERROR = 1e-5
+
+    export
+        #data and tree definition    
+        TransNet,
+        Tborg
+    include("betnet1.0.jl")
+end # module
