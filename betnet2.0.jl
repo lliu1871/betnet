9f3b33677f72681dd599c34f8f07f128d8bb290e
lliu1871 <lliu@uga.edu>
2026-03-22 08:52:58 -0400

    update README.md

--- betnet2.0.jl
+++
@@ -1,5 +0,0 @@
-
-
-using betnet
-@time transNetworkInference(tempfile="./data/TemporalData1_100.csv",SNPfile="./data/SNP1_100.csv",Contactfile="./data/ContactProb1_100.csv",genomeSize=1000000, itr_MCMC=1000000, burn_in=0,subsample=1000, outputfile="parameter_betnet_net.csv")
-
