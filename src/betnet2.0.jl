d9e60a16a453a512851cd0c033a723646852a359
lliu1871 <lliu@uga.edu>
2026-03-31 19:14:03 -0400

    correction for tie onsite times

--- src/betnet2.0.jl
+++ src/betnet2.0.jl
@@ -282,7 +282,14 @@ function moveInfector()
                 error("Error: negative latent time!")
             end
         end
-        diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
+        # check if latent time is 0, if yes, use a small value to calculate logprior to avoid log(0)
+        if latent_time_new == 0.0
+            diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), 1.0e-10)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
+        elseif latent_time_old == 0.0
+             diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), 1.0e-10))
+        else
+            diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
+        end #diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
     else
         latent_time_old = data.tempData[individual_update,1] - InfTime_old
         latent_time_new = data.tempData[individual_update,1] - InfTime_new 
@@ -291,7 +298,15 @@ function moveInfector()
                 error("Error: negative latent time!")
             end
         end
-        diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
+        # check if latent time is 0, if yes, use a small value to calculate logprior to avoid log(0)
+        if latent_time_new == 0.0
+            diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), 1.0e-10)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
+        elseif latent_time_old == 0.0
+             diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), 1.0e-10))
+        else
+            diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
+        end
+        #diff_logprior = log(pdf(Chisq(parameters.latent_priorMean), latent_time_new)) - log(pdf(Chisq(parameters.latent_priorMean), latent_time_old))
         diff_logprior += log(parameters.ContactProb[individual_update,infector_new]) - log(parameters.ContactProb[individual_update,infector_old])   
         diff_logprior += log(pdf(Geometric(parameters.removalRate/(parameters.infectionRate+parameters.removalRate)),parameters.Child_Vec[infector_new]+1)) +
                      log(pdf(Geometric(parameters.removalRate/(parameters.infectionRate+parameters.removalRate)),parameters.Child_Vec[infector_old]-1)) -
@@ -349,7 +364,13 @@ function mcmcAlgorithm(outputfile::String)
                 error("Error: negative latent time!")
             end
         end
-        mcmc.logPrior += log(pdf(Chisq(parameters.latent_priorMean), latent_time)) - log(cdf(Chisq(parameters.latent_priorMean), parameters.InfectionPeriod[infID,j])) + log(parameters.ContactProb[infID,j])
+        # if latent_time == 0, we use a small value 1.0e-10 to calculate the logprior to avoid log(0)
+        if(latent_time == 0.0)
+            mcmc.logPrior += log(pdf(Chisq(parameters.latent_priorMean), 1.0e-10)) - log(cdf(Chisq(parameters.latent_priorMean), 1.0e-10)) + log(parameters.ContactProb[infID,j])
+        else
+            mcmc.logPrior += log(pdf(Chisq(parameters.latent_priorMean), latent_time)) - log(cdf(Chisq(parameters.latent_priorMean), parameters.InfectionPeriod[infID,j])) + log(parameters.ContactProb[infID,j])
+        end      
+        #mcmc.logPrior += log(pdf(Chisq(parameters.latent_priorMean), latent_time)) - log(cdf(Chisq(parameters.latent_priorMean), parameters.InfectionPeriod[infID,j])) + log(parameters.ContactProb[infID,j])
     end
     
     for round in 1:mcmc.numIter       
